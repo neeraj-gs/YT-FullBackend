52b814ed150db6b7bb8706224e22128e4dcf2d81
Neeraj GS <gsneeraj05@gmail.com>
2024-02-26 08:44:31 +0530

    Added logout controller to remove the access and refesj tokenss in cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -169,6 +169,25 @@ const logoutUser = asyncHandler(async (req, res) => {
     //we use middleware when we cant get user 
 
     //by adding cookie parser we can access cookies and access it in req and res
+    await User.findByIdAndUpdate(
+        req.user._id, //find
+        {
+            //updation
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true, //returned response we get new value
+        }
+    )
+
+    const options = {
+        httpOnly: true, 
+        secure:true,
+    }
+
+    return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new ApiResponse(200,{},"User Logged Out Successfully"))
 
 
 })
@@ -186,4 +205,4 @@ const logoutUser = asyncHandler(async (req, res) => {
 
 
 
-export {registerUser,loginUser}
+export {registerUser,loginUser,logoutUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
-import { registerUser } from '../controllers/user.controller.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.js'
+import {verifyJWT} from '../middlewares/auth.middleware.js'
 
 const router = Router();
 
@@ -19,5 +20,18 @@ router.route("/register").post(
 
     ]), //fields accepts array
     registerUser)
+
+
+router.route("/login").post(loginUser)
+
+//routes when user is logged in only then rotue it 
+//Secured Routes
+router.route("/logout").post(verifyJWT,logoutUser)
+//when we have a middleware then we get a secured route , if not present then we will re route them back
+
+
+
+
+
 
 export default router
